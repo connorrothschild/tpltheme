b57773fd5e1fae5254c7d1e1ea33c0f5fe42c981
Connor Rothschild <connorrothschild@gmail.com>
2019-06-28 13:38:47 -0500

    create texas plot

--- R/theme_tpl_texas.R
+++ R/theme_tpl_texas.R
@@ -9,4 +9,38 @@
 #' @param base_family,base_size base font family and size
 #' @param base_line_size,base_rect_size base line and rectangle sizes
 #' @export
+#'
 
+### TO DO
+
+theme_tpl_texas <- function(scale = "continuous",
+                           base_size = 8.5,
+                           base_family = "Adobe Caslon Pro",
+                           base_line_size = 0.5,
+                           base_rect_size = 0.5) {
+
+  gg <- theme_urbn_print(base_size = 8.5, base_family = "Adobe Caslon Pro",
+                         base_line_size = 0.5,
+                         base_rect_size = 0.5)
+
+  gg <- gg +  ggplot2::theme(
+
+    # make changes for mapping styles
+    axis.text = ggplot2::element_blank(),
+    axis.ticks = ggplot2::element_blank(),
+    axis.title = ggplot2::element_blank(),
+    panel.grid = ggplot2::element_blank(),
+    axis.line = ggplot2::element_blank(),
+    legend.margin = ggplot2::margin(t = 6L, r = 6L, b = 6L, l = 6L, "pt")
+  )
+
+  if (scale == "continuous") {
+    gg <- gg + ggplot2::theme(
+      legend.position = "right",
+      legend.direction = "vertical",
+      legend.title = ggplot2::element_text(size = base_size)
+    )
+  }
+  # return gg
+  gg
+}
