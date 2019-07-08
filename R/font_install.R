e87f15a4022dbfff578ee8990d7898602e7629e2
Connor Rothschild <connorrothschild@gmail.com>
2019-07-08 12:12:15 -0500

    update font scripts to include Lato in addition to Adobe Caslon Pro

--- R/font_install.R
+++ R/font_install.R
@@ -1,16 +1,17 @@
-#' Import and register Adobe Caslon Pro font
+#' Import and register Adobe Caslon Pro & Lato font
 #'
-#' \code{font_install} tests to see if Adobe Caslon Pro is imported and registered. If Adobe Caslon Pro isn't imported and registered, then \code{font_install} imports and registers Adobe Caslon Pro with \href{https://github.com/wch/extrafont}{library(extrafont)}.
+#' \code{font_install} tests to see if default fonts imported and registered. If Adobe Caslon Pro & Lato are not imported and registered, then \code{font_install} imports and registers Adobe Caslon Pro & Lato with \href{https://github.com/wch/extrafont}{library(extrafont)}.
 #'
-#' Note: Adobe Caslon Pro must be installed on your computer for \code{font_install} to work.
+#' Note: Adobe Caslon Pr must be installed on your computer for \code{font_install} to work.
 #'
 #' Test to see if Adobe Caslon Pro is imported and registered with \code{font_test()}.
 #'
 #' @md
 #' @export
 font_install <- function() {
-  if (sum(grepl("[Aa]dobe [Cc]aslon [Pp]ro$", extrafont::fonts())) > 0) {
-    "Adobe Caslon Pro is already imported and registered."
+  if (sum(grepl("[Aa]dobe [Cc]aslon [Pp]ro$", extrafont::fonts())) > 0 &&
+      sum(grepl("[Ll]ato$", extrafont::fonts())) > 0) {
+    "Available fonts (Adobe Caslon Pro and Lato) already imported and registered."
   } else {
     extrafont::font_import()
     extrafont::loadfonts()
--- R/font_test.R
+++ R/font_test.R
@@ -1,15 +1,18 @@
-#' Test for Adobe Caslon Pro import and registration
+#' Test for Adobe Caslon Pro & Lato import and registration
 #'
-#' \code{font_test} tests to see if Adobe Caslon Pro is imported and registered.
+#' \code{font_test} tests to see if default fonts are imported and registered.
 #'
-#' Import and register Adobe Caslon Pro in R with font_install().
+#' Import and register Adobe Caslon Pro & Lato in R with font_install().
 #'
 #' @md
 #' @export
 font_test <- function() {
-  if (sum(grepl("[Aa]dobe [Cc]aslon [Pp]ro$", extrafont::fonts())) > 0) {
-    "Adobe Caslon Pro is imported and registered."
-  } else {
+  if ((sum(grepl("[Aa]dobe [Cc]aslon [Pp]ro$", extrafont::fonts())) > 0) &&
+    sum(grepl("[Ll]ato$", extrafont::fonts())) > 0) {
+    "Default fonts are imported and registered."
+  } else if (sum(grepl("[Ll]ato$", extrafont::fonts())) == 0) {
+    "Lato isn't imported and registered. Install the font online and import and register using font_install()."
+  } else if (sum(grepl("[Aa]dobe [Cc]aslon [Pp]ro$", extrafont::fonts())) == 0) {
     "Adobe Caslon Pro isn't imported and registered. Install the font online and import and register using font_install()."
   }
 }
