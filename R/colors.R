f3edf2b648708423b9969da1b3121b40d7cec347
Connor Rothschild <connorrothschild@gmail.com>
2019-07-08 08:59:00 -0500

    add color palettes for rice, school of social sciences

--- R/colors.R
+++ R/colors.R
@@ -1,12 +1,12 @@
 #' Texas Policy Lab color palettes
-#' 
+#'
 #' Vectors with hex-color codes that correspond to the color palettes found on the Texas Policy Lab website
 #' \url{https://www.texaspolicylab.org/}
-#' 
+#'
 #' @source Texas Policy Lab website
 #'   \url{https://www.texaspolicylab.org/}
-#'   
-#'   
+#'
+#'
 #'@family TPL palettes
 #'@rdname TPL_palletes
 #'@export
@@ -14,3 +14,14 @@ palette_TPL_main <- c(
   blue = "#151348",
   red = "#E54E4D",
   lightblue = "#5393EA")
+
+palette_school_soc_sci <- c(
+  blue = "#004080",
+  gray = "#DDDDDD",
+  white = "#ffffff"
+)
+
+palette_rice <- c(
+  blue = "#005499",
+  gray = "#505050"
+)
