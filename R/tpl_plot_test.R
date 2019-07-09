b88a00ed0207b0a17609020059fbdabdb3a618a6
Connor Rothschild <connorrothschild@gmail.com>
2019-07-09 13:51:36 -0500

    add clarity to the type argument

--- R/tpl_plot_test.R
+++ R/tpl_plot_test.R
@@ -1,30 +1,30 @@
-#' Sample plots to test the appearance of \code {theme_TPL_print}
-#' 
+#' Sample plots to test the appearance of \code{theme_TPL_print}
+#'
 #' @import ggplot2
-#' 
+#'
 #' @example \dontrun{
 #' tpl_plot_test(type = "scatterplot")
 #' }
 #' @export
-#' @param type Type of plot to be produced
+#' @param type Type of plot to be produced. Options include \code{barplot}, \code{scatterplot}, \code{boxplot}, and \code{histogram}
 #' @param include.logo Whether to include the TPL logo at the bottom of the plot
 tpl_plot_test <- function(type = "barplot", include.logo = F) {
- 
-  #set theme 
+
+  #set theme
   set_tpl_theme(style = "print", font = "adobe")
-  
+
   #select plot type
     if (type == "barplot") {
-  plot <- ggplot(iris, aes(x=Species, y=Sepal.Width, fill=Species)) + 
-    geom_bar(stat="summary", fun.y="mean") + 
+  plot <- ggplot(iris, aes(x=Species, y=Sepal.Width, fill=Species)) +
+    geom_bar(stat="summary", fun.y="mean") +
     labs(x="Species", y="Mean Sepal Width (cm)", fill="Species", title="Iris Dataset")
   } else if (type == "scatterplot") {
-  plot <- ggplot(iris, aes(x=jitter(Sepal.Width), y=jitter(Sepal.Length), col=Species, size = Petal.Length)) + 
-    geom_point() + 
+  plot <- ggplot(iris, aes(x=jitter(Sepal.Width), y=jitter(Sepal.Length), col=Species, size = Petal.Length)) +
+    geom_point() +
     labs(x="Sepal Width (cm)", y="Sepal Length (cm)", col="Species", title="Iris Dataset")
   } else if (type == "boxplot") {
-  plot <- ggplot(iris, aes(x=Species, y=Sepal.Width, fill=Species)) + 
-    geom_boxplot() + 
+  plot <- ggplot(iris, aes(x=Species, y=Sepal.Width, fill=Species)) +
+    geom_boxplot() +
     labs(x="Species", y="Sepal Width (cm)", fill="Species", title="Iris Dataset")
   } else if (type == "histogram") {
     plot <- ggplot(iris, aes(x=Sepal.Width)) +
@@ -39,10 +39,10 @@ tpl_plot_test <- function(type = "barplot", include.logo = F) {
       plot <- gridExtra::grid.arrange(plot, tpl_logo_text(), ncol = 1, heights = c(30, 1))
     } else if (include.logo == F) {
       return(plot)
-    } 
+    }
     return(plot)
   }
-  
+
 # tpl_plot_test(type = "barplot", include.logo = T)
 # tpl_plot_test(type = "boxplot", include.logo = T)
 # tpl_plot_test(type = "scatterplot", include.logo = F)
