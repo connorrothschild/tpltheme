79c8ba684130ef8613483a5bb2f6882dc9363878
Connor Rothschild <connorrothschild@gmail.com>
2019-07-10 10:11:14 -0500

    add tpl_logo_text() to the add_tpl_logo function. Include an argument "align" which allows the user to specify the level of horizontal alignment the TPL logo should follow.

--- R/add_tpl_logo.R
+++ R/add_tpl_logo.R
@@ -9,12 +9,28 @@
 #' @import gridExtra
 #' @md
 #' @export
-add_tpl_logo <- function(plot) {
+add_tpl_logo <- function(plot, align = 0) {
+  tpl_logo_text <- function() {
+
+    grid::grobTree(
+      gp = grid::gpar(fontsize = 11, hjust = 1),
+      grid::textGrob(label = " POLICY LAB",
+                     name = "caption1",
+                     x = unit(1 + align/10, "npc"),
+                     y = unit(1, "npc"),
+                     hjust = 2.2,
+                     vjust = 0,
+                     gp = grid::gpar(col = config$palettes$tpl_main$blue,
+                                     fontfamily = "Adobe Caslon Pro")),
+      grid::textGrob(label = "TEXAS",
+                     x = unit(1 + align/10, "npc") - grid::grobWidth("caption1") - unit(0.001, "lines"),
+                     y = unit(1, "npc"),
+                     hjust = 3.3,
+                     vjust = 0,
+                     gp = grid::gpar(col = config$palettes$tpl_main$red,
+                                     fontfamily = "Adobe Caslon Pro"))
+    )
+  }
+
     plot <- gridExtra::grid.arrange(plot, tpl_logo_text(), ncol = 1, heights = c(30, 1))
 }
-
-# plot <- ggplot(iris, aes(x=Species, y=Sepal.Width, fill=Species)) +
-#   geom_col() +
-#   labs(x="Species", y="Mean Sepal Width (cm)", fill="Species", title="Iris Dataset")
-#
-# add_tpl_logo(plot)
