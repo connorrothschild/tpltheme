2d274f46d689bfae548adc1a9058d2e21a76c6d5
Connor Rothschild <connorrothschild@gmail.com>
2019-07-11 14:00:46 -0500

    updated colors: blue -> oxford blue and red -> carmine pink

--- R/add_tpl_logo.R
+++ R/add_tpl_logo.R
@@ -20,14 +20,14 @@ add_tpl_logo <- function(plot, align = 0) {
                      y = unit(1, "npc"),
                      hjust = 2.2,
                      vjust = 0,
-                     gp = grid::gpar(col = config$palettes$tpl_main$blue,
+                     gp = grid::gpar(col = config$palettes$tpl_main$oxfordblue,
                                      fontfamily = "Adobe Caslon Pro")),
       grid::textGrob(label = "TEXAS",
                      x = unit(1 + align/10, "npc") - grid::grobWidth("caption1") - unit(0.001, "lines"),
                      y = unit(1, "npc"),
                      hjust = 3.3,
                      vjust = 0,
-                     gp = grid::gpar(col = config$palettes$tpl_main$red,
+                     gp = grid::gpar(col = config$palettes$tpl_main$carminepink,
                                      fontfamily = "Adobe Caslon Pro"))
     )
   }
