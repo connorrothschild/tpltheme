da575e86115c679c68c438f0be293408cd63a164
Connor Rothschild <connorrothschild@gmail.com>
2019-07-11 13:59:16 -0500

    create view_palette, which allows the user to explore different options for TPL colors

---
+++ R/view_palette.R
@@ -0,0 +1,44 @@
+#' Texas Policy Lab [ggplot2] theme
+#'
+#' \code{view_palette} displays the colors and hexadecimal codes for \code{palette_tpl_*} vectors.
+#' These palettes were generated on [https://coolors.co/].
+#'
+#' @param palette A \code{palette_tpl_*} vector from \code{library(tpltheme)}.
+#'   Options are `palette_tpl_diverging`, `palette_tpl_politics`,
+#'   `palette_tpl_quintile`, `palette_tpl_cyan`, `palette_tpl_gray`,
+#'   `palette_tpl_yellow`, `palette_tpl_magenta`, `palette_tpl_green`,
+#'   `palette_tpl_spacegray`, and `palette_tpl_red`.
+#'
+#' @examples
+#' view_palette()
+#' view_palette(palette_tpl_cyan)
+#'
+#' @md
+#'
+#' @export
+view_palette <- function(palette = palette_tpl_main) {
+
+  color_palette <- unname(rev(palette))
+
+  data <- tibble::tibble(x = 1,
+                         y = 1:length(color_palette),
+                         colors = factor(color_palette, levels = color_palette))
+
+  ggplot2::ggplot(data = data) +
+    ggplot2::geom_point(ggplot2::aes_string("x", "y", color = "colors"), size = 15) +
+    ggplot2::geom_text(ggplot2::aes_string(2, "y"), label = color_palette) +
+    ggplot2::scale_color_manual(values = color_palette) +
+    ggplot2::scale_x_continuous(limits = c(0, 3)) +
+    theme_tpl_texas() +
+    ggplot2::guides(color = FALSE)
+
+}
+
+# #option 1 for tpl categorical
+# view_palette(palette = palette_tpl_main)
+# #option 2 for tpl categorical
+# view_palette(palette = palette_tpl_cat)
+# #diverging
+# view_palette(palette = palette_tpl_diverging)
+# #sequential
+# ?
