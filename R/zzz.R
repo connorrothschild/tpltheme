23d7c3bbc74f37b9bd5500add54144a44e378257
Connor Rothschild <connorrothschild@gmail.com>
2019-07-16 16:20:42 -0500

    comment out bad zzz lines

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,8 @@
 .onLoad <- function(libname, pkgname) {
 
-  config <- yaml::read_yaml("https://raw.githubusercontent.com/connorrothschild/tpltheme/master/config.yaml")
-  setwd("/Users/connorrothschild/Desktop/R/TPL/tpltheme")
-  config <- yaml::read_yaml(file.path("config.yaml"))
-  #source("packrat/init.R")
+  # config <- yaml::read_yaml("https://raw.githubusercontent.com/connorrothschild/tpltheme/master/config.yaml")
+  # setwd("/Users/connorrothschild/Desktop/R/TPL/tpltheme")
+  # config <- yaml::read_yaml(file.path("config.yaml"))
+  # source("packrat/init.R")
 
 }
