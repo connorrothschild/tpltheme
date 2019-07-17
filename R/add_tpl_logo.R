0e70f7d76d8c0b497ee907f3bcec4d1488d95e90
Connor Rothschild <connorrothschild@gmail.com>
2019-07-17 11:31:02 -0500

    change regular to large, make default logo size small. more significant change: change from grob_image to magick::image_read so that the function uses a URL rather than local file path

--- R/add_tpl_logo.R
+++ R/add_tpl_logo.R
@@ -3,7 +3,7 @@
 #' Add TPL logo to a single plot as grid object
 #'
 #' @example \dontrun{
-#' add_tpl_logo(plot, size = "regular", position = "bottomright", align = .5)
+#' add_tpl_logo(plot, size = "large", position = "bottomright", align = .5)
 #' }
 #'
 #' @param plot The plot object passed to `add_tpl_logo`
@@ -14,66 +14,84 @@
 #' @import gridExtra
 #' @md
 #' @export
-add_tpl_logo <- function(plot, size = "regular", position = "top", align = 0) {
+add_tpl_logo <- function(plot, size = "small", position = "top", align = 0) {
 
-  img <- "https://raw.githubusercontent.com/connorrothschild/tpltheme/master/figures/TPL_Logo_Full_TPL_Logo_2_Color.png"
-  utils::download.file(img, 'figures/logo.png', mode = 'wb')
+  # set default height and width
+
+  dheight = 287
+  dwidth = 1156
+
+  # image and separator object
+
+  logo <- magick::image_read('https://raw.githubusercontent.com/connorrothschild/tpltheme/master/figures/logo.png')
+  logo <- magick::image_trim(logo)
   separator <- grid::rectGrob(gp=grid::gpar(col="white"))
 
-    if (size == "regular" && position != "bottomleft") {
+  # read image in
 
-      groblogo <- grobblR::grob_image("./figures/logo.png", aes_list = NULL, height = 15, width = 54)
-      finalgrob <- gridExtra::arrangeGrob(separator, groblogo, separator, widths = c(15 + align,10 - align,2))
+    if (size == "large") {
+
+    logo <- magick::image_scale(logo, dwidth/4)
+
+    } else if (size == "small") {
+
+    logo <- magick::image_scale(logo, dwidth/3)
+
+    }
+
+  groblogo <- grid::rasterGrob(logo)
+
+    if (size == "large" && position != "bottomleft") {
+
+      finalgrob <- gridExtra::arrangeGrob(separator, groblogo, separator, widths = c(15 + align, 15 - align, 2))
 
     } else if (size == "small" && position != "bottomleft") {
 
-      groblogo <- grobblR::grob_image("./figures/logo.png", aes_list = NULL, height = 10, width = 36)
-      finalgrob <- gridExtra::arrangeGrob(separator, groblogo, separator, widths = c(30 + align,10 - align,5))
+      finalgrob <- gridExtra::arrangeGrob(separator, groblogo, separator, widths = c(30 + align, 10 - align,5))
 
     } else if (position == "bottomleft") {
-       if (size == "regular") {
 
-         groblogo <- grobblR::grob_image("./figures/logo.png", aes_list = NULL, height = 15, width = 54)
-         finalgrob <- gridExtra::arrangeGrob(groblogo, separator, widths = c(12 + align,20 - align))
+      if (size == "large") {
+
+         finalgrob <- gridExtra::arrangeGrob(separator, groblogo, separator, widths = c(2, 12 + align, 20 - align))
 
        } else if (size == "small") {
 
-         groblogo <- grobblR::grob_image("./figures/logo.png", aes_list = NULL, height = 10, width = 36)
-         finalgrob <- gridExtra::arrangeGrob(groblogo, separator, widths = c(10 + align,24 - align))
+         finalgrob <- gridExtra::arrangeGrob(separator, groblogo, separator, widths = c(2, 6 + align, 18 - align))
 
        } else {
-         stop("Size specification not found. Options are 'small' and 'regular.",
+         stop("Size specification not found. Options are 'small' and 'large.",
               call. = FALSE)
        }
 
     }
 
     if (position == "top") {
-      if (size == "regular") {
-      plot <- gridExtra::grid.arrange(finalgrob, plot, ncol = 1, heights = c(5, 30))
+      if (size == "large") {
+      plot <- gridExtra::grid.arrange(finalgrob, plot, ncol = 1, heights = c(4, 35))
       } else if (size == "small") {
-        plot <- gridExtra::grid.arrange(finalgrob, plot, ncol = 1, heights = c(3, 20))
+        plot <- gridExtra::grid.arrange(finalgrob, plot, ncol = 1, heights = c(3, 25))
       } else {
-        stop("Size specification not found. Options are 'small' and 'regular.",
+        stop("Size specification not found. Options are 'small' and 'large.",
              call. = FALSE)
       }
     } else if (position == "bottomright") {
-      if (size == "regular") {
-        plot <- gridExtra::grid.arrange(plot, finalgrob, ncol = 1, heights = c(30, 5))
+      if (size == "large") {
+        plot <- gridExtra::grid.arrange(plot, finalgrob, ncol = 1, heights = c(35, 4))
       } else if (size == "small") {
-        plot <- gridExtra::grid.arrange(plot, finalgrob, ncol = 1, heights = c(20, 3))
+        plot <- gridExtra::grid.arrange(plot, finalgrob, ncol = 1, heights = c(25, 3))
       } else {
-        stop("Size specification not found. Options are 'small' and 'regular.",
+        stop("Size specification not found. Options are 'small' and 'large.",
              call. = FALSE)
       }
     } else if (position == "bottomleft") {
 
-      if (size == "regular") {
-        plot <- gridExtra::grid.arrange(plot, finalgrob, ncol = 1, heights = c(30, 5))
+      if (size == "large") {
+        plot <- gridExtra::grid.arrange(plot, finalgrob, ncol = 1, heights = c(30, 4.5))
       } else if (size == "small") {
-        plot <- gridExtra::grid.arrange(plot, finalgrob, ncol = 1, heights = c(20, 3))
+        plot <- gridExtra::grid.arrange(plot, finalgrob, ncol = 1, heights = c(25, 3))
       } else {
-        stop("Size specification not found. Options are 'small' and 'regular.",
+        stop("Size specification not found. Options are 'small' and 'large.",
              call. = FALSE)
     }
       } else {
@@ -83,7 +101,7 @@ add_tpl_logo <- function(plot, size = "regular", position = "top", align = 0) {
   return(plot)
 }
 
-# add_tpl_logo(tpl_plot_test(type = "scatterplot"), size = "regular", position = "bottomright", align = .5)
+#add_tpl_logo(tpl_plot_test(type = "scatterplot"), size = "small", position = "bottomleft", align = 0)
 
 ## Other option (not preferred) ========
 # plot <- ggplot(iris, aes(x=jitter(Sepal.Width), y=jitter(Sepal.Length), col=Species, size = Petal.Length)) +
