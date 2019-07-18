7ba400e5d4b089615ea5e4bce0f1cfe528780b04
Connor Rothschild <connorrothschild@gmail.com>
2019-07-18 15:37:19 -0500

    fix duplicates

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(libname, pkgname) {
 
-  scales <- c("scale_color_discrete", "scale_colour_ordinal", "scale_colour_discrete", "scale_color_continuous", "scale_color_gradient", "scale_colour_gradient",
-              "scale_color_gradient", "scale_colour_gradient", "scale_fill_discrete", "scale_fill_ordinal", "scale_fill_continuous", "scale_fill_gradient", "scale_fill_gradient")
+  scales <- c("scale_color_discrete", "scale_colour_ordinal", "scale_colour_discrete", "scale_color_continuous", "scale_color_gradient",
+              "scale_colour_gradient", "scale_fill_discrete", "scale_fill_ordinal", "scale_fill_continuous", "scale_fill_gradient")
 
   for (i in scales) {
   conflicted::conflict_prefer(name   =  i,
