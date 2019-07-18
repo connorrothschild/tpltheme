d7af1f9cfa556771410ee844ddf745df619d7228
Connor Rothschild <connorrothschild@gmail.com>
2019-07-18 15:27:26 -0500

    add multiple functions to conflict_prefer

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,17 @@
 .onLoad <- function(libname, pkgname) {
 
-  conflicted::conflict_prefer("tpltheme", "ggplot2")
+  scales <- c("scale_color_discrete", "scale_colour_ordinal", "scale_colour_discrete", "scale_color_continuous", "scale_color_gradient", "scale_colour_gradient",
+              "scale_color_gradient", "scale_colour_gradient", "scale_fill_discrete", "scale_fill_ordinal", "scale_fill_continuous", "scale_fill_gradient", "scale_fill_gradient")
+
+  for (i in scales) {
+  conflicted::conflict_prefer(name   =  i,
+                              winner ="tpltheme",
+                              loser  = "ggplot2")
+  }
+
   # config <- yaml::read_yaml("https://raw.githubusercontent.com/connorrothschild/tpltheme/master/config.yaml")
   # setwd("/Users/connorrothschild/Desktop/R/TPL/tpltheme")
   # config <- yaml::read_yaml(file.path("config.yaml"))
   # source("packrat/init.R")
 
 }
-
