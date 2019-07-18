c1ae775e615b0b3ba5444796065b2a4f9809e6f7
Connor Rothschild <connorrothschild@gmail.com>
2019-07-18 15:14:42 -0500

    add conflicted to resolve namespace issues between ggplot and tpltheme

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,10 @@
-# .onLoad <- function(libname, pkgname) {
-#
-#   # config <- yaml::read_yaml("https://raw.githubusercontent.com/connorrothschild/tpltheme/master/config.yaml")
-#   # setwd("/Users/connorrothschild/Desktop/R/TPL/tpltheme")
-#   # config <- yaml::read_yaml(file.path("config.yaml"))
-#   # source("packrat/init.R")
-#
-# }
+.onLoad <- function(libname, pkgname) {
+
+  conflicted::conflict_prefer("tpltheme", "ggplot2")
+  # config <- yaml::read_yaml("https://raw.githubusercontent.com/connorrothschild/tpltheme/master/config.yaml")
+  # setwd("/Users/connorrothschild/Desktop/R/TPL/tpltheme")
+  # config <- yaml::read_yaml(file.path("config.yaml"))
+  # source("packrat/init.R")
+
+}
+
