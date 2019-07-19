a218735ba44fad149f68b0d3706bb88ce1576c2a
Connor Rothschild <connorrothschild@gmail.com>
2019-07-19 11:36:48 -0500

    add geoms to conflicted:: conflicts

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,8 @@
 .onLoad <- function(libname, pkgname) {
 
   scales <- c("scale_color_discrete", "scale_colour_ordinal", "scale_colour_discrete", "scale_color_continuous", "scale_color_gradient",
-              "scale_colour_gradient", "scale_fill_discrete", "scale_fill_ordinal", "scale_fill_continuous", "scale_fill_gradient")
+              "scale_colour_gradient", "scale_fill_discrete", "scale_fill_ordinal", "scale_fill_continuous", "scale_fill_gradient",
+              "geom_bar", "geom_col", "geom_jitter", "geom_line", "geom_step", "geom_path")
 
   suppressMessages(
     for (i in scales) {
